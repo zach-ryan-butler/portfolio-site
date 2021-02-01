2e5f22084bafa99981e21d95fc85d3606cbd5c44
Zach Butler <zach.standley93@gmail.com>
2021-02-01 11:17:30 -0800

    add material-ui-core and active navlink styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+import { NavHashLink as Link } from 'react-router-hash-link';
 
 import styles from './header.css';
 
 export default function Header() {
   return (
     <div className={styles.root}>
-      <Link smooth to='/#section-home'>
+      <Link
+        activeClassName='selected'
+        activeStyle={{ color: 'yellow' }}
+        smooth
+        to='/#section-home'
+      >
         Home
       </Link>
-      <Link smooth to='/about#section-about'>
+      <Link
+        activeClassName='selected'
+        activeStyle={{ color: 'yellow' }}
+        smooth
+        to='/about#section-about'
+      >
         About
       </Link>
-      <Link smooth to='/projects#section-projects'>
+      <Link
+        activeClassName='selected'
+        activeStyle={{ color: 'yellow' }}
+        smooth
+        to='/projects#section-projects'
+      >
         Projects
       </Link>
-      <Link smooth to='/resume#section-resume'>
+      <Link
+        activeClassName='selected'
+        activeStyle={{ color: 'yellow' }}
+        smooth
+        to='/resume#section-resume'
+      >
         Resume
       </Link>
-      <Link smooth to='/contact#section-contact'>
+      <Link
+        activeClassName='selected'
+        activeStyle={{ color: 'yellow' }}
+        smooth
+        to='/contact#section-contact'
+      >
         Contact
       </Link>
     </div>
